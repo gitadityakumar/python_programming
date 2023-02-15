cac45be23da760ec25ec7fd8ce1335c87e317329
Aditya kumar <adityasingh05@gmail.com>
2023-02-16 01:29:41 +0530

    space seprated list

--- Arrays & Lists/takinginputinlist/linesepratedinput.py
+++ Arrays & Lists/takinginputinlist/linesepratedinput.py
@@ -1,6 +1,7 @@
 n = 5
 li = []
 for i in range(n):
-    curr = (input())
+    # if we remove int we can take input string // basically by  defing input data  type we take input wwhat we want!
+    curr = int(input())
     li.append(curr)
 print(li)
---
+++ Arrays & Lists/takinginputinlist/space_seperated_input.py
@@ -0,0 +1,4 @@
+# space seprated list by taking input from user
+li = [int(x) for x in input().split()]
+for ele in li:
+    print(ele)
